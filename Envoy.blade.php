fd1f459607ef933aa9d0f926da36c6babf0fcd25
hari <thejems12@gmail.com>
2020-09-12 22:57:01 +0700

    update envoy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,5 +1,37 @@
-@servers(['local' => ['127.0.0.1']])
-
-@task('foo', ['on' => 'local'])
-    ls -la
+@servers(['web' => '192.168.1.103'])
+@setup
+    $repository = 'git@github.com:memble/laravel-test-deploy.git';
+    $releases_dir = '/var/www/releases';
+    $app_dir = '/var/www/';
+    $release = date('dmYHis');
+    $new_release_dir = $releases_dir .'/'. $release;
+@endsetup
+@story('deploy')
+    clone_repository
+    run_composer
+    update_symlinks
+@endstory
+@task('clone_repository')
+    echo 'Cloning repository'
+    [ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
+    git clone --depth 1 {{ $repository }} {{ $new_release_dir }}
+    echo 'Done'
+@endtask
+@task('run_composer')
+    echo "Starting deployment ({{ $release }})"
+    cd {{ $new_release_dir }}
+    composer install --prefer-dist --no-scripts -q -o
+    echo 'Done'
+@endtask
+@task('update_symlinks')
+    echo "Linking storage directory"
+    rm -rf {{ $new_release_dir }}/storage
+    ln -nfs {{ $app_dir }}/storage {{ $new_release_dir }}/storage
+    echo 'Done'
+    echo 'Linking .env file'
+    ln -nfs {{ $app_dir }}/.env {{ $new_release_dir }}/.env
+    echo 'Done'
+    echo 'Linking current release'
+    ln -nfs {{ $new_release_dir }} {{ $app_dir }}/current
+    echo 'Done'
 @endtask
